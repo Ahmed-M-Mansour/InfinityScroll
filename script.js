f7f8de8718d8d3ec5d40c3ed51362d579fa14cfc
Ahmed Mansour <ahmedmmansour18@gmail.com>
2021-03-15 00:54:43 +0200

    first version of app

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 const imageContainer = document.getElementById('image-container') ; 
 const looder = document.getElementById('looder'); 
 
+let ready = false ; 
+let imagesLoaded = 0 ; 
+let totalImages = 0 ; 
 let phtpArray = [] ; 
 
 // Unsplash API 
 const apiKey = 'EnU5pZJuHC9lRw7Bmi5O-F27Zsi7XNCsKEBms9kJsrM' ; 
-const count = 1 ; 
+const count = 3 ; 
 const url = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}` ; 
 
+
+// check if the image is loaded 
+function imageLoaded() {
+    imagesLoaded++ ; 
+    if(imagesLoaded === totalImages ) 
+    {
+        ready = true ; 
+        looder.hidden = true ; 
+    }
+  }
 // to create elements for links and images , then  Add it to the DOM 
 function displayPhotos(){
+    imagesLoaded = 0 ; 
+    totalImages = phtpArray.length ; 
+    console.log('Total Images --' , totalImages );
     // Run function for each object in phtpArray
     phtpArray.forEach((photo)=>{
         // create <a>  to link to Unsplash website 
@@ -21,13 +37,13 @@ function displayPhotos(){
         img.setAttribute('src' , photo.urls.regular); 
         img.setAttribute('alt' , photo.alt_description) ; 
         img.setAttribute('title' , photo.alt_description) ;  
+         // Event lissitner , check when ach function is loading 
+         img.addEventListener('load' , imageLoaded) ; 
         // put image iside <a> , then put the both inside image container 
         item.appendChild(img); 
         imageContainer.appendChild(item);
     })
 }
-
-
 // get photosfrom an  API 
  async function getPhotos(){
     try {
@@ -35,13 +51,18 @@ function displayPhotos(){
         phtpArray = await response.json(); 
         console.log(phtpArray) ; 
         displayPhotos(); 
-
     }catch(erorr) {
         // catch erorr here 
         console.log(erorr);
     }
  }
 
-
+// checking to see if scrolling near the botton 
+window.addEventListener('scroll' , ()=>{
+    if(window.innerHeight + window.scrollY > document.body.offsetHeight - 1000 && ready ){
+        ready = false ; 
+        getPhotos() ; 
+    }
+})
 //  on looding 
 getPhotos() ; 
